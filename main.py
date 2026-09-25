bafc50ab436670f3dc5c87155bd89ae9454faf69
jorgehenriqueEG <deusnopeito31@gmail.com>
2026-09-24 22:42:02 -0300

    feat: add source code

---
+++ main.py
@@ -0,0 +1,12 @@
+valor_base = 50
+s_small = 4
+s_medium = 2
+s_large = 1
+
+mult_small = 1.0
+mult_medium = 1.5
+mult_large = 2.0
+
+total = (s_small * mult_small + s_medium * mult_medium + s_large * mult_large) * valor_base
+
+print("Salário total:", total)
